6d71e8b83e3db02f30a43c52b82aca8f90617636
[yejinj] <80540445+yejinj@users.noreply.github.com>
2024-08-21 16:00:37 +0900

    [feat] models 폴더 생성 및 사용자 정보 데이터베이스화

---
+++ models/User.js
@@ -0,0 +1,25 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = new Sequelize({
+  dialect: 'sqlite',
+  storage: 'database.sqlite'
+});
+
+const User = sequelize.define('User', {
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+});
+
+sequelize.sync();
+
+module.exports = User;
